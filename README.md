4c61cd00fdb678002c0d9399448443e20627a685
Lukas Riha <oswlriha@oriflame.com>
2022-06-07 09:46:15 +0200

    Something

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Actum Frontend
 
 Might need to run `chmod +x .husky/pre-commit` (happened to me on mac. not sure what causes it)
+
+Do I want to add stylelint?
+
+> I wish I could. Well can, but won't.
+> Should maybe, but shorn't.
+> Michael Gary Scott
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import Document from "next/document";
 import { ServerStyleSheet } from "styled-components";
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default class CustomDocument extends Document {
     static async getInitialProps(ctx) {
@@ -21,5 +22,29 @@ export default class CustomDocument extends Document {
         } finally {
             sheet.seal();
         }
+    }
+
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.googleapis.com"
+                    />
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.gstatic.com"
+                        crossOrigin="anonymous"
+                    />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Archivo:wght@400;500;700&display=swap"
+                        rel="stylesheet"
+                    />
+                </Head>
+                <Main />
+                <NextScript />
+            </Html>
+        );
     }
 }
