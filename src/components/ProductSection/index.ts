9b0476fe8c6d9aea4a6b0ab1a0919f85c07cffcf
Lukas Riha <lukas.riha94@seznam.cz>
2022-06-09 13:55:08 +0200

    Mobile filter

--- src/components/ProductSection/index.ts
+++ src/components/ProductSection/index.ts
@@ -1,2 +1,6 @@
 export * from "./ProductSection";
 export * from "./ProductsHeading";
+export * from "./DesktopFilters";
+export * from "./MobileFilterToggle";
+export * from "./Sort";
+export * from "./Products";
