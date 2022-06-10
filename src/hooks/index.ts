97357be2ec26c86215dd08e6f2ce98641d3b81d6
Lukas Riha <lukas.riha94@seznam.cz>
2022-06-10 14:50:25 +0200

    removed some unsused files

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,5 +2,4 @@ export * from "./useCartContext";
 export * from "./useFilterContext";
 export * from "./useSortContext";
 export * from "./useIsMobile";
-export * from "./useActiveProductsContext";
 export * from "./useProducts";
--- src/hooks/useActiveProductsContext.ts
+++
@@ -1,4 +0,0 @@
-import { useContext } from "react";
-import { ActiveProductsContext } from "../context";
-
-export const useActiveProductsContext = () => useContext(ActiveProductsContext);
